808dd48fffa0b149acf7a06fb8f98ebea91cef64
Ziek <86315179+ziekdjd@users.noreply.github.com>
2026-08-27 13:15:55 +0800

    Add files via upload

---
+++ Area of a Circle/Area of a Circle/Area of a Circle.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	double radius, PI = 3.1416;
+
+	cout << "Enter Value for Radius: ";
+	cin >> radius;
+
+	cout << "The area of a circle is: " << PI * (radius * radius);
+}
